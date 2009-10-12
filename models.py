dcb0ab5df64dfac3427978ac3c6484961108383f
Unknown <sean@clyde.localdomain>
2009-10-12 10:53:15 -0400

    Comitting before changing param

--- models.py
+++ models.py
@@ -25,18 +25,18 @@ class ObserveState0(noise.Gauss):
         self.Times = EventTimed()
         self.sigma = 0.0001
         
-    def mean(self, time, state, param):
+    def mean(self, time, state):
         return state[0, 0]
         
-    def meas(self, time, state, param):
-        return self.mean(time, state, param) + self.sigma*self.eval(time)
+    def meas(self, time, state):
+        return self.mean(time, state) + self.sigma*self.eval(time)
         
-    def Dstate(self, time, state, param):
+    def Dstate(self, time, state):
         J = numpy.matrix(numpy.zeros((1, state.shape[0])))
         J[0, 0] = 1
         return J
         
-    def Dnoise(self, time, state, param):
+    def Dnoise(self, time, state):
         return numpy.matrix(self.sigma)
 
 # This class defines a list of the observation classes employed.
@@ -67,40 +67,40 @@ class ObservationModel:
         return E
 
     # Evaluate the mean measurement at a given time
-    def mean(self, time, state, param, List=None):
+    def mean(self, time, state, List=None):
         E = []
         if List == None:
             List = range(0, self.D)
         for k in List:        
-            E.append(self.C[k].mean(time, state, param).tolist())
+            E.append(self.C[k].mean(time, state).tolist())
         return numpy.matrix(E)
 
     # Evaluate the measurement at a given time
-    def meas(self, time, state, param, List=None):
+    def meas(self, time, state, List=None):
         E = []
         if List == None:
             List = range(0, self.D)
         for k in List:
-            E.append(self.C[k].meas(time, state, param).tolist())
+            E.append(self.C[k].meas(time, state).tolist())
         return numpy.matrix(E)
         
-    def Dstate(self, time, state, param, List=None):
+    def Dstate(self, time, state, List=None):
         if List == None:
             List = range(0, self.D)
         E = numpy.matrix(numpy.zeros((len(List), state.shape[0])))
         Eindex = 0
         for k in List:
-            E[Eindex, :] = self.C[k].Dstate(time, state, param)
+            E[Eindex, :] = self.C[k].Dstate(time, state)
             Eindex += 1
         return E
         
-    def Dnoise(self, time, state, param, List=None):
+    def Dnoise(self, time, state, List=None):
         if List == None:
             List = range(0, self.D)
         E = numpy.matrix(numpy.zeros((len(List), len(List))))
         Eindex = 0
         for k in List:
-            E[Eindex, Eindex] = self.C[k].Dnoise(time, state, param)
+            E[Eindex, Eindex] = self.C[k].Dnoise(time, state)
             Eindex += 1
         return numpy.matrix(E) 
         
@@ -124,22 +124,22 @@ class DecayModel:
         return E
 
     # Evaluate the mean vector field at a given time
-    def vfield(self, time, state,  param, discrete=None):
-        return -param.A*state
+    def vfield(self, time, state, discrete=None):
+        return -self.P.A*state
     
-    def flow(self, Times, state0, param, discrete=None):
+    def flow(self, Times, state0, discrete=None):
         EndTime = Times[len(Times)-1]
         if len(state) == 1:
-            return (math.exp(-param.A*(EndTime-Times[0])))*state0
+            return (math.exp(-self.P.A*(EndTime-Times[0])))*state0
         else:
-            return (scipy.linalg.expm(-param.A*(EndTime-Times[0])))*state0
+            return (scipy.linalg.expm(-self.P.A*(EndTime-Times[0])))*state0
     
-    def Dstate(self, Times, state0, param, discrete=None):
+    def Dstate(self, Times, state0, discrete=None):
         EndTime = Times[len(Times)-1]
         if len(state) == 1:
-            return (math.exp(-param.A*(EndTime-Times[0])))
+            return (math.exp(-self.P.A*(EndTime-Times[0])))
         else:
-            return (scipy.linalg.expm(-param.A*(EndTime-Times[0])))
+            return (scipy.linalg.expm(-self.P.A*(EndTime-Times[0])))
             
-    def Dnoise(self, Times, state, param, discrete=None):
+    def Dnoise(self, Times, state, discrete=None):
  
