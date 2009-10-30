eda2368f0f6e4448b7a84cc544b4d0dd365330af
Michael Hines <michael.hines@yale.edu>
2009-10-30 15:29:38 -0400

    2 state decay in nrn. consistent with runTwoDecay.py

---
+++ nrn2decay.py
@@ -0,0 +1,14 @@
+from neuron import h
+from math import pi
+h.load_file('nrngui.hoc')
+ss = [h.Section(), h.Section()]
+for s in ss:
+  s.diam = 10
+  s.L = 100/pi/s.diam
+  s.insert('pas')
+  s.e_pas = 0
+ss[0].g_pas = .5*.001
+ss[1].g_pas = .1*.001
+h.tstop = 20
+h.dt = 0.1
+h.v_init = 1
--- nrnlinalg.py
+++ nrnlinalg.py
@@ -1,4 +1,5 @@
 from neuron import h
+import numpy
 
 #Determinant
 def det(m):
@@ -8,3 +9,16 @@ def det(m):
   e = h.ref(0)  
   d = hm.det(e)  
   return d*10.0**e[0]
+
+def expm(m):
+  hm = h.Matrix(m.shape[0], m.shape[1])
+  for i in range(int(hm.nrow())):
+    hm.setcol(i, h.Vector(m[:,i]))
+  hm = hm.exp()
+  mo = numpy.matrix(m)
+  print m
+  
+  for i in range(int(hm.nrow())):
+    for j in range(int(hm.nrow())):
+      mo[i,j] = hm.getval(i, j)
+  return mo
