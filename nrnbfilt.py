3f617b7f01a8d2e5b03b776d0c0038ca106262b3
Unknown <sean@clyde.localdomain>
2009-11-05 13:21:33 -0500

    __data programed in NrnBFilt.py

--- nrnbfilt.py
+++ nrnbfilt.py
@@ -1,8 +1,8 @@
 from neuron import h
 
 class NrnBFilt(object):
-  def __init__(self, ho):
-    self.rf = ho
+	def __init__(self, ho):
+		self.rf = ho
     ol = []
     P = noise.NoiseParam()
     P.dt = 0.1
@@ -26,7 +26,18 @@ class NrnBFilt(object):
     Sys = models.NeuronModel(P, 0, len(vl))
     Sys.Injection.erange(0.0, tlast, 1.0)
     self.M = models.Model(Sys, Obs, P)
-    self.Data = __data(fl)
-
-  def likelihood(self):
+    self.Data = __data(fl,self.M.FitEvents)
+    
+  def __data(self,fl,FitEvents):
+		# counter = [0]*(len(fl))
+		Data = []
+		for ev = range(len(FitEvents)):
+			DataEV = []
+		  for ob = range(len(FitEvents[ev][1])):
+				DataEv.append(fl.o[FitEvents[ev][1][ob]])
+				#?????
+			Data.append(numpy.matrix(DataEv))
+		return Data
+		
+		def likelihood(self):
     return fitEKF.ekf(self.Data, self.M)
