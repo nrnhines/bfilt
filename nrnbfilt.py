1c69ea681b88f0f8ec5838aa344c2b9a75001317
Michael Hines <michael.hines@yale.edu>
2009-11-13 09:55:24 -0500

    return negative log likelihood, turn on cvode earlier, both default noise is 0.01,

--- nrnbfilt.py
+++ nrnbfilt.py
@@ -3,6 +3,7 @@ import noise
 import models
 import numpy
 import fitEKF
+import math
 
 class NrnBFilt(object):
   def __init__(self, ho):
@@ -16,24 +17,25 @@ class NrnBFilt(object):
     for i in range(len(vl)):
       tl = list(fl.o(i).xdat_)
       o = models.NeuronObservable(vl.o(i), P, i, tl)
-      o.sigma = 0.0001
+      o.sigma = 0.01
       ol.append(o)
       if (tlast < tl[-1]):
         tlast = tl[-1]
     P.tstop = tlast
     s = h.Vector()
+    h.cvode.active(1)
     h.cvode.states(s)
     assert(len(s) > 0)
     assert(len(vl) > 0)
     P.B = numpy.matrix(numpy.zeros((len(s), len(vl))))
-    P.B[0,0] = .1
+    P.B[0,0] = .01
     P.InitialCov = numpy.eye(len(s))
     Obs = models.ObservationModel(P, 1000, ol)
     Sys = models.NeuronModel(P, 0, len(vl))
     Sys.Injection.erange(0.0, tlast, 1.0)
     self.M = models.Model(Sys, Obs, P)
     self.Data = self.__data(fl,self.M.FitEvents)
-    print 'leave NrnBFilt'
+    #print 'leave NrnBFilt'
 
   def __data(self,fl,FitEvents):
     counter = [1]*(len(fl))
@@ -45,7 +47,8 @@ class NrnBFilt(object):
       for i in obindices:
         x = fl.o(i).xdat_
         y = fl.o(i).ydat_
-        assert(time == x[counter[i]])
+        print i, counter[i], time, x[counter[i]], time - x[counter[i]]
+        assert(math.fabs(time - x[counter[i]]) < 1e-10)
         DataEV.append(y[counter[i]])
         counter[i] += 1
       Data.append(numpy.matrix(DataEV))
@@ -54,7 +57,6 @@ class NrnBFilt(object):
     return Data
 		
   def likelihood(self):
-    print 'likelihood'
     x = fitEKF.ekf(self.Data, self.M)
     x = float(x)
-    return x
+    return -x
