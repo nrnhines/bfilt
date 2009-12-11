307ab3263222a1c68269ddb3b9434de77bd65d11
Unknown <sean@callisto>
2009-12-11 10:53:28 -0500

    EKF just needs Predict

--- EKF.py
+++ EKF.py
@@ -4,7 +4,8 @@ import numpy
 import fitglobals
 
 def initializeErrorBars():
-    global Etime, Ecenter, Ewidth
+    global saveErrorBars, Etime, Ecenter, Ewidth
+    saveErrorBars = True
     Etime = []
     Ecenter = []
     Ewidth = []   
@@ -18,13 +19,14 @@ def initialStateCov(model):
     P0 = model.P.InitialCov
     return (m0, P0)
     
-def modelMeasurement(time,ObsNum,m,P,saveErrorBars):
+def modelMeasurement(time,ObsNum,m,P):
 # Returns the measurement and movariance plus Jacobians
     hh = model.Obs.mean(time, m, ObsNum)
     H = model.Obs.Dstate(time, m, ObsNum)
     V = model.Obs.Dnoise(time, m, ObsNum)
     S = H*P*H.T + V*V.T
     
+    global saveErrorBars
     if saveErrorBars:
         global Etime, Ecenter, Ewidth
         Etime.append(time0)
@@ -33,36 +35,40 @@ def modelMeasurement(time,ObsNum,m,P,saveErrorBars):
             Ewidth[ObsNum[iObs]].append(math.sqrt(S[iObs,iObs]))
     return (hh,S,H,V)
 
-def update(data,time,ObsNum,mb,Pb,saveErrorBars)
-    (hh,S,H,V) = modelMeasurement(time,ObsNum,mb,Pb,saveErrorBars):
+def update(data,time,ObsNum,mb,Pb):
+    (hh,S,H,V) = modelMeasurement(time,ObsNum,mb,Pb):
     e = data - hh
     K = Pb*H.T*S.I
     P = Pb - K*S*K.T
     m = mb + K*e
+    modelMeasurement(time,ObsNum,m,P)
     return (m,P,e,S)
 
-def predict(m,P,t0,t1)
-    # NEED TO WRITE THIS FUNCTION
+def predict(m,P,t0,t1,injectionTimes):
+    return (mb, Pb, t1)
     
+def minusTwiceLogGaussianPDF(v,S):
+    f0 = len(v)*math.log(2*math.pi)
+    f1 =  math.log(linalg.det(S))
+    f2 = (v.T*S.I*v).tolist()[0][0]
+    return (f0+f1+f2)
+
 def ekf(data, model):
     # Initialize
+    smll = 0
     initializeErrorBars()
     (m0, P0) = initialStateCov(model)
-    k = 0
-    smll = 0
-    SaveErrorBars = True
-    previousTime = 0
     
     # Main loop
-    while(k<len(data)):
-        if k == 0 and collectionTimes[0] == 0:
-            (m,P,e,S) = update(data[0],collectionTimes[0],ObsNums[0],m0,P0,SaveErrorBars)
-            modelMeasurement(collectionTimes[0],ObsNums[0],m,P,SaveErrorBars)
-            k = 1
-        else:
-            (m,P) = (m0,P0)
-        (mb,Pb) = predict(m,P,previousTime,collectionTimes[k])
-        (m,P,e,S) = update(data[k],collectionTimes[k],ObsNums[k],mb,Pb,SaveErrorBars)
-        modelMeasurement(collectionTimes[k],ObsNums[k],m,P,SaveErrorBars)
-        previousTime = collectionTimes[k]
+    if collectionTime[0] == 0:
+        (m,P,e,S) = update(data[0],collectionTime[0],ObsNums[0],m0,P0)
+        k = 1
+    else:
+        (m,P) = (m0,P0)
+        k = 0
+    while(k<len(data))        
+        (mb,Pb,time) = predict(m,P,time,collectionTime[k],injectionTimes[k])
+        (m,P,e,S) = update(data[k],collectionTime[k],ObsNum[k],mb,Pb,saveError)
+        smll += minusTwiceLogGaussianPDF(e,S)
         k += 1
+    return -smll/2.0
