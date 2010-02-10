90f846dd5078960d1ba7235d5de4662b593084cf
Unknown <sean@clyde>
2010-02-10 09:46:58 -0500

    Got Gaussian sampler working

--- BSF.py
+++ BSF.py
@@ -1,4 +1,17 @@
 import numpy
 import scipy.linalg as linalg
+import random
 
-def sample(m,C):
+class Sample(object):
+    R = random.Random()
+
+    def seed(self, i):
+        self.R.seed(i)
+
+    def sample(self, m, C):
+        A = linalg.cholesky(C)
+        x = [0]*len(m)
+        for i in range(len(m)):
+            x[i] = self.R.normalvariate(0,1)
+        x = numpy.matrix(x)
+        return m + A*x.T
