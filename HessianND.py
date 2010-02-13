f0ff4973d0f2c36e9ce8853f78f0794842bd97d0
Unknown <sean@clyde>
2010-02-13 11:39:05 -0500

    HessianND functional

--- HessianND.py
+++ HessianND.py
@@ -1,20 +1,47 @@
 import numdifftools as nd
 
-def evalFun(fun, args, p)
-    # ######### TO_DO write getMRFparam
-    saveParams = getMRFparam();
-    # ######### TO_DO write setMRFparam
-    setMRFparam(p)
-    # ######### TO_DO check syntax
-    L = fun(args)
-    setMRFparam(saveParams)
+def evalFun(nbf, p):
+    saveParm = nbf.getParm()
+    newParm = nbf.getParm()
+    for i in range(len(p)):
+        newParm.x[i] = p[i]
+    nbf.setParm(newParm)
+    L = nbf.likelihood()
+    nbf.setParm(saveParm)
+    return L
 
-def HessianMRF(fun,args):
+def Hessian(nbf):
     # Read current values of parameters (hopefully MLEs)
-    param = getMRFparam();
+    parm = nbf.getParm()
+    parmList = []
+    for i in range(int(parm.size())):
+        parmList.append(parm[i])
     # Create (likelihood) inline function
-    LamFun = lambda p: evalFun(fun,args,p)
+    LamFun = lambda p: evalFun(nbf,p)
     # Create Hessian (of likelihood) inline function
     HessFun = nd.Hessian(LamFun)
     # Evaluate Hessian and return
-    return likeHess(param)
+    return HessFun(parmList)
+
+def Gradient(nbf):
+    # Read current values of parameters (hopefully MLEs)
+    parm = nbf.getParm()
+    parmList = []
+    for i in range(int(parm.size())):
+        parmList.append(parm[i])
+    # Create (likelihood) inline function
+    LamFun = lambda p: evalFun(nbf,p)
+    # Create Hessian (of likelihood) inline function
+    GradFun = nd.Gradient(LamFun)
+    # Evaluate Hessian and return
+    return GradFun(parmList)
+
+def LikePerturbed(nbf,perturbList,delta):
+    saveParm = nbf.getParm()
+    newParm = nbf.getParm()
+    for i in range(len(perturbList)):
+        newParm.x[i] = newParm[i] + delta*perturbList[i]
+    nbf.setParm(newParm)
+    L = nbf.likelihood()
+    nbf.setParm(saveParm)
+    return L
