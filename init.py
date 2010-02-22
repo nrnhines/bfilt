8b6982700040da660f9fde859a4403d782d4e26a
Michael Hines <michael.hines@yale.edu>
2010-02-22 14:37:26 -0500

    allow factoring out the model into another directory and launching from their Need an export PYTHONPATH=/home/hines/neuron/bfilt then can launch with nrngui $PYTHONPATH/init.hoc hhss.ses where the ses file defines the model and multiple run fitter. Also can launch nrngui -python $PYTHONPATH/init.py and use neuronmainmenu to load the ses file

--- init.py
+++ init.py
@@ -2,5 +2,12 @@ from neuron import h
 import fitglobals
 fitglobals.debugoff()
 h.load_file('mulfit.hoc')
-h.load_file('eonerunmlf.hoc')
 import nrnbfilt
+
+import sys
+n = sys.modules['nrnbfilt'].__str__()
+n = n[n.find('/'):n.rfind('/')]
+if len(n) and n[-1] != '/':
+    n += '/'
+h.load_file(n+'eonerunmlf.hoc')
+del n
