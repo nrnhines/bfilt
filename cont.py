f588605268a12575d191d693bb9c3235076eecfb
Unknown <sean@callisto>
2010-03-05 14:59:51 -0500

    cont.py now correctly follows contours of test circle function

--- cont.py
+++ cont.py
@@ -8,11 +8,10 @@ tolx = numpy.inf
 minstep = 1e-5
 
 def jac(FUN,args,n,fv=None):
-  # print 'jac a:', args
-  # print 'jac an:', n
+  # print 'jac args:', args
+  # print 'jac n:', n
   # print 'jac FUN', FUN
   if fv == None:
-    print 'fv args', args
     fv = FUN(*args)
   DF = numpy.matrix(numpy.zeros([1,len(args[n])]))  # nongeneral should be len(fv)
   for j in range(len(args[n])):
@@ -21,7 +20,8 @@ def jac(FUN,args,n,fv=None):
       h = sqrteps*abs(temp)
     else:
       h=sqrteps
-    x = args[n][:]
+    # print 'args[n]', args[n]
+    x = args[n].copy()
     x[j] = temp+h
     h = x[j] - temp  # trick to reduce finite precision error
     arglist = list(args)
@@ -34,7 +34,7 @@ def jac(FUN,args,n,fv=None):
 
 def jac1(FUN,args,n,ic,fv=None):
   # print 'jac 1', [args[n][ic]]
-  cargs = ([args[n][ic]],FUN,args,n,ic)
+  cargs = (numpy.matrix([[args[n][0,ic]]]),FUN,args,n,ic)
   DF = jac(cFUN,cargs,0,fv)  # derivative wrt 0th comp of args
   return DF
 
@@ -69,47 +69,37 @@ def newtn(FUN, x, args): # returns (x, fx, its)
       return (x,fx,i)
 
     Df = jac(FUN,(x,)+args,0,fx)
-    dx = - Df.I*fx
+    dx = -Df.I*fx
     x = x + dx
+    # print 'Df', Df
+    # print 'Df.I', Df.I
+    # print 'fx', fx
+    # print 'dx', dx
 
   i = numpy.inf
   print ' Maximum number of iterations exceeded'
   return (x,fx,i)
 
 
-def ctest(a0):
-  pa = [numpy.matrix(a0)]
-  print 'pa & pa[-1]', pa, pa[-1]
-  return pa
-
-def cctest():
-  pa = conttest(1)
-  return pa
-
 def cont(FUN, x0, a0, args, ic, aicf, step):
   global minstep
-  (x,fx,its)  = newtn(FUN, x0, (a0,)+args)
+  (x,fx,its)  = newtn(FUN, numpy.matrix(x0), (a0,)+args)
   assert(its < numpy.inf)
 
-  a = a0
+  a = numpy.matrix(a0)
   px = [numpy.matrix(x)]
   pa = [numpy.matrix(a0)]
-  print 'px & px[-1]', px, px[-1]
-  print 'pa & pa[-1]', pa, pa[-1]
-  temp = pa
-  print 'temp & temp[-1]', temp, temp[-1]
-  return (px,pa)
 
-  print 'px', px
-  print 'pa', pa
+  # print 'px', px
+  # print 'pa', pa
 
-  if aicf == a[ic]:
+  if aicf == a[0,ic]:
     return (px,pa)
-  elif (aicf - a[ic])*step < 0:
+  elif (aicf - a[0,ic])*step < 0:
     step = step * (-1)
   atend = False
 
-  print 'x', x
+  # print 'x', x
   DxF = jac(FUN,(x,a)+args,0)
   DaF = jac1(FUN,(x,a)+args,1,ic)
   # print 'DxF', DxF
@@ -125,21 +115,19 @@ def cont(FUN, x0, a0, args, ic, aicf, step):
     tx = DaX/nt
     ta = 1/nt
 
-    print 'px & px[-1]', px, px[-1]
-    print 'pa & pa[-1]', pa, pa[-1]
-    temp = pa
-    print 'temp & temp[-1]', temp, temp[-1]
-    return (px,pa)
-    x0 = px[-1]
-    a0 = pa[-1]
+    # print 'px & px[-1]', px, px[-1]
+    # print 'pa & pa[-1]', pa, pa[-1]
+
+    x0 = px[-1].copy()
+    a0 = pa[-1].copy()
     its = numpy.inf
 
     while its == numpy.inf:
       x = x0 + step*tx
+      a = a0.copy()
+      a[0,ic] = a[0,ic] + step*ta
       print 'a', a
       print 'a0', a0
-      a[0,ic] = a0[0,ic] + step*ta
-
       if (aicf - a[0,ic])*step <= 0:
         atend = True
         laststep = step
@@ -167,12 +155,18 @@ def cont(FUN, x0, a0, args, ic, aicf, step):
         print 'Increment to contin variable less than MINSTEP; exiting ...'
         return (px,pa)
 
-    px.append(x)
-    pa.append(a)
-    print 'add px', px
-    print 'add pa', pa
+    px.append(x.copy())
+    pa.append(a.copy())
+    # print 'add px', px
+    # print 'add pa', pa
 
   return (px,pa)
+
+def xypoints(px,pa):
+  xy = []
+  for i in range(len(px)):
+    xy.append([px[i][0,0],pa[i][0,0]])
+  return xy
 
 def testFUN(x,a,val):
   # print 'test x', x
--- ctest.py
+++ ctest.py
@@ -1,5 +1,11 @@
 import numpy
-def ctest(a0):
-  pa = [numpy.matrix(a0)]
-  print 'pa & pa[-1]', pa, pa[-1]
-  return pa
+def ctest(a0, recurse=False):
+    if recurse:
+        pa = ctest(a0,False)
+    else:
+        a = numpy.matrix(a0)
+        pa = []
+        pa.append(a)
+        pa.append(a)
+    print 'pa & pa[-1] & pa[0] & pa[1]', pa, pa[-1],  pa[0], pa[1]
+    return pa
