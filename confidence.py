9972e6326a59fade39ce1b62730c963d34bfbd74
Unknown <sean@clyde>
2010-03-11 12:26:42 -0500

    trying to get confidence Hessian to work, not yet successful

--- confidence.py
+++ confidence.py
@@ -3,6 +3,7 @@ import math
 from neuron import h
 import scipy.stats as stats
 import scipy.optimize as optimize
+import numpy
 
 def evalFun(nbf, p):
     saveParm = nbf.getParm()
@@ -56,9 +57,20 @@ def test4(Parm):
     return HessFun(Parm)
 
 def rememberMLE(nbf):
-    global ML, MLE
+    global H, ML, MLE
     MLE = nbf.getParm()
     ML = nbf.likelihood()
+    H = Hessian(nbf)
+
+def return2MLE(nbf):
+    global MLE
+    nbf.setParm(MLE)
+
+def pValue(nbf,v):
+    global ML
+    nbf.setParm(v)
+    CS = 2.0*(nbf.likelihood() - ML)
+    return stats.chisqprob(CS,v.size())
 
 def pMinusAlpha(nbf,v,alpha):
     global ML
@@ -89,44 +101,74 @@ def perturbedEval(pert,nbf,dir,alpha):
     # print '4:there', v[1]
     return pMinusAlpha(nbf,v,alpha)
 
-def polarEval(r,theta,nbf,alpha):
+def likeEval(r,theta,nbf,alpha):
     global MLE
     d = h.Vector(2)
     d.x[0] = MLE[0] + r*math.cos(theta)
     d.x[1] = MLE[1] + r*math.sin(theta)
     return pMinusAlpha(nbf,d,alpha)
 
+def hessEval(r,theta, nbf, alpha=0.0):
+    global H, MLE
+    x = r*math.cos(theta)
+    y = r*math.sin(theta)
+    HI = numpy.matrix(H).I
+    xy = numpy.matrix([[x],[y]])
+    CS = xy.T*HI*xy
+    return stats.chisqprob(CS,2)-alpha
+
+def hessTest(List):
+    global H,ML
+    HI = numpy.matrix(H).I
+    copyList = List[:]
+    for xy in copyList:
+        xy = numpy.matrix(xy).T
+        xy[0] -= MLE[0]
+        xy[1] -= MLE[1]
+        CS = xy.T*HI*xy
+        print 'p-value:', stats.chisqprob(CS,2)
+
 def polarFind(theta, nbf, alpha):
-    global MLE
+    global MLE, funEval
     nbf.setParm(MLE)
     r0 = 1
-    pe = polarEval(r0,theta,nbf,alpha)
+    pe = funEval(r0,theta,nbf,alpha)
     print 'pe', pe
     while pe > 0.0:
         r0 = r0*5
-        pe = polarEval(r0,theta,nbf,alpha)
+        pe = funEval(r0,theta,nbf,alpha)
         print 'peI', pe
         assert(r0 < 500000)
-    print 'f(a)', polarEval(0,theta,nbf,alpha)
-    print 'f(b)', polarEval(r0,theta,nbf,alpha)
-    r = optimize.brentq(polarEval,0,r0,(theta,nbf,alpha))
+    print 'f(a)', funEval(0,theta,nbf,alpha)
+    print 'f(b)', funEval(r0,theta,nbf,alpha)
+    r = optimize.brentq(funEval,0,r0,(theta,nbf,alpha))
     return r
 
-def polarInitial(r0,theta,nbf,alpha):
+def vectorFind(theta,nbf,alpha):
     global MLE
+    r = polarFind(theta,nbf,alpha)
+    v = h.Vector(2)
+    v.x[0] = MLE[0] + r*math.cos(theta)
+    v.x[1] = MLE[1] + r*math.sin(theta)
+    return v
+
+def polarInitial(r0,theta,nbf,alpha):
+    global MLE, funEval
     nbf.setParm(MLE)
-    z = polarEval(r0,theta,nbf,alpha)
+    z = funEval(r0,theta,nbf,alpha)
     if z > 0.0:
         a = r0
         b = r0*1.1
-        while polarEval(b,theta,nbf,alpha) > 0:
+        while funEval(b,theta,nbf,alpha) > 0:
             b= b*1.1
+            assert(b<1e9)
     elif z<0.0:
         b = r0
         a = r0*0.9
-        while polarEval(a,theta,nbf,alpha) < 0:
+        while funEval(a,theta,nbf,alpha) < 0:
             a = a*0.9
-    r = optimize.brentq(polarEval,a,b,(theta,nbf,alpha))
+            assert(a>1e-9)
+    r = optimize.brentq(funEval,a,b,(theta,nbf,alpha))
     return r
 
 
@@ -135,7 +177,10 @@ def polarContinue(theta0, nbf, alpha, points):
     rt = [[r,theta0]]
     for i in range(points):
         theta = theta0 + 2*math.pi*(i+1)/points
+        return2MLE(nbf)
+        print 'point #', i
         r = polarInitial(r,theta,nbf,alpha)
+        # r = polarFind(theta, nbf, alpha)
         rt.append([r,theta])
     return rt
 
@@ -148,19 +193,23 @@ def polar2xy(rt,origin=None):
         M = origin
         xy.append([M[0],M[1]])
     for rtheta in rt:
-        xy.append([M[0] + rtheta[0]*math.cos(rtheta[1]), M[1] + rtheta[1]*math.sin(rtheta[1])])
+        xy.append([M[0] + rtheta[0]*math.cos(rtheta[1]), M[1] + rtheta[0]*math.sin(rtheta[1])])
     return xy
+
+def log2linear(List):
+    linxy = []
+    for xy in List:
+        linxy.append([math.exp(xy[0]), math.exp(xy[1])])
+    return linxy
 
 def testxy(nbf, List):
     for xy in List:
-        p = nbf.getParm()
+        p = h.Vector(2)
         p.x[0] = xy[0]
         p.x[1] = xy[1]
         nbf.setParm(p)
-        like = nbf.likelihood()
-        print 'xy', xy
-        print 'L', like
-        print 'p-alpha', pMinusAlpha(nbf,p,0.05)
+        pv = pValue(nbf,p)
+        print 'pv', pv
 
 def findSignificant(nbf,alpha):
     global ML, MLE
@@ -181,3 +230,5 @@ def findSignificant(nbf,alpha):
     p = optimize.brentq(perturbedEval,0,pert,(nbf,dir,alpha))
     dir.mul(p)
     return dir
+
+funEval = hessEval
