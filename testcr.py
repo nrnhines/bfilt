a8c31a1b650f3d7e6f202d170f0acd12714779ad
Unknown <sean@clyde>
2010-06-08 15:20:13 -0400

    testcr: modified single channel generator

--- testcr.py
+++ testcr.py
@@ -4,25 +4,36 @@ import init
 from neuron import h
 import noisegen
 import scipy.stats as stats
+import numpy
 
 class TestCR(object):
     def __init__(self,n,ses,seed):
         self.alpha = 0.05
         self.n = n
-        self.true = self.N.getParm()
         h.load_file(ses)
-        self.ses = ses
         self.N = h.List("PythonObject").o(0)
+        self.true = self.N.getParm()
+        self.ses = ses
         if n == 0:
           G = noisegen.Gen(self.N)
           G.reseed(seed)
           self.seed = seed
           self.Data = G.datasim()
         else:
-          vec = ch3ssdata(n, seed, datavec, true)
-          self.Data = numpy.matrix(vec)
+          h.load_file("ch3ssdatagen.hoc")
+          tvec = h.Vector(self.N.Eve.collectionTimes)
+          vec = h.ch3ssdata(n, seed, tvec, self.true)
+          self.Data = []
+          for i in range(len(vec)):
+            self.Data.append(numpy.matrix(vec[i]))
+        h.topology()
+        ss = h.Vector()
+        h.cvode.states(ss)
+        ss.printf()
         self.N.overwrite(self.Data)
         self.tl = self.N.likelihood()
+        print self.tl
+        return
         h.MulRunFitter[0].efun()
         self.mle = self.N.getParm()
         self.ml = self.N.likelihood()
