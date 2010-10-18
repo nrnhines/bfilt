d9c4a1bce02020d5d0a82f57d38d1830868b3c31
Unknown <sean@callisot>
2010-10-18 11:46:21 -0400

    Adding 11 and 101 point trajectories

---
+++ ch3_101p.py
@@ -0,0 +1,10 @@
+from neuron import h
+import fitglobals
+fitglobals.debugoff()
+h.load_file('mulfit.hoc')
+h.load_file('eonerunmlf.hoc')
+import nrnbfilt
+h.load_file('ch3_101p.ses')
+
+h('objref nb')
+h.nb = h.List("PythonObject").o(0)
