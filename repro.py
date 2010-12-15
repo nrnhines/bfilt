8f4ef1cbd868f5e6cb683354f0b5d475a999c791
Unknown <sean@clyde>
2010-12-14 22:42:16 -0500

    repro programmed for channels but need debugging

--- repro.py
+++ repro.py
@@ -6,9 +6,9 @@ import os
 import os.path
 
 class Repro(object):
-    def __init__(self, modelses, parm=None, channels=0, seed=0):
+    def __init__(self, model, parm=None, channels=0, seed=0):
         # parm = None: saves parm to what's saved in session on first datagen
-        self.modelses = modelses
+        self.model = model
         self.parm = parm
         self.channels = channels
         self.seed = seed
@@ -18,20 +18,25 @@ class Repro(object):
         self.timestamp = time.time()
 
     def datagen(self):
-        ses_timestamp = os.path.getmtime(self.modelses)
+        ses_timestamp = os.path.getmtime(self.model)
         assert(ses_timestamp < self.timestamp)
-        h.load_file(self.modelses)  # load session file
-        mrflist = h.List("MulRunFitter")
-        mrf = mrflist.o(int(mrflist.count())-1)
-        N = mrf.p.pf.generatorlist.o(0).gen.po
-        if self.parm == None:
-            self.parm = N.getParm()
-        else:
-            N.setParm(self.parm)
         if self.channels == 0:
+            h.load_file(self.model)  # load session file
+            mrflist = h.List("MulRunFitter")
+            mrf = mrflist.o(int(mrflist.count())-1)
+            N = mrf.p.pf.generatorlist.o(0).gen.po
+            if self.parm == None:
+                self.parm = N.getParm()
+            else:
+                N.setParm(self.parm)
             G = noisegen.Gen(N)
             G.reseed(self.seed)
             Data = G.datasim()
             return Data
         else:
-            print "Channel noise not implemented"
+            h.load_file(self.model)  # e.g. self.model = exper_channel.hoc
+            h.load_file("exper_data.hoc")
+            h.set_true_parameters()
+            self.parm = h.true_parameters()
+            Data = h.experimentalDataGenerator(self.channels, self.seed)
+            return Data
