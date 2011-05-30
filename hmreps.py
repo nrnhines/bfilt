db7a5a91943b29eb3c53cf768875d9fd58194110
Unknown <sean@callisto>
2011-05-30 15:00:31 -0400

    hmreps now saves mles

--- hmreps.py
+++ hmreps.py
@@ -5,15 +5,16 @@ def getp(seed):
     print "seed", seed
     K = hmlike.HML()
     K.sim(seed)
-    return (K.p_true(),K.gopt)
+    return (K.p_true(),K.gopt,K.mle)
 
 def reps(n):
     ps = []
     gs = []
     for seed in range(n):
-        (p,g) = getp(seed)
+        (p,g,x) = getp(seed)
         ps.append(p)
         gs.append(g)
+        xs.append(x)
         print 'gtol', g
     fname = "reps"+str(n)+".pkl"
     f = open(fname,'w')
