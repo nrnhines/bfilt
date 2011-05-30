8fb48576ede5a618b34f60de8c565a82040bbbfd
Unknown <sean@callisto>
2011-05-30 01:18:47 -0400

    Added saving gtol to reps.py

--- hmreps.py
+++ hmreps.py
@@ -5,14 +5,18 @@ def getp(seed):
     print "seed", seed
     K = hmlike.HML()
     K.sim(seed)
-    return K.p_true()
+    return (K.p_true(),K.gtol)
 
 def reps(n):
     ps = []
+    gs = []
     for seed in range(n):
-        p = getp(seed)
+        (p,g) = getp(seed)
         ps.append(p)
+        gs.append(g)
+        print 'gtol', g
     fname = "reps"+str(n)+".pkl"
     f = open(fname,'w')
     pickle.dump(ps,f)
+    pickle.dump(gs,f)
     f.close
