049758d23e742d11ea5749a45ec71fc6b17c83e3
seancarver <unknown>
2011-07-11 10:05:55 -0700

    HR version of conSeed2 ready to run

--- hmRun.py
+++ hmRun.py
@@ -1,11 +1,11 @@
 import hmExperiment
 import numpy
 
-def conSeed2():
-    rn = numpy.arange(.01,15.001,.5).tolist()
+def conSeed2HR():
+    rn = numpy.arange(.001,12.0001,.05).tolist()
     F = hmExperiment.fit(hmExperiment.ch3up,dict(tau01=2.,tau12=4.),dict(nchannels=5))
     F.sim(hmExperiment.ch3up,dict(tau01=2.,tau12=4.,nchannels=5),seeds=[[2]],tstops=[[20]])
-    F.save4plot("conSeed2",dict(tau01=rn,tau12=rn))
+    F.save4plot("conSeed2HR",dict(tau01=rn,tau12=rn))
     print F.find()
     
     
